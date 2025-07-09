48eb127986fa0cce1c61b7a07d40052b95df3d1a
Luis Mario Cabrera Torres <104865952+MarioCab07@users.noreply.github.com>
2025-07-08 20:41:46 -0600

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
-INSERT INTO usuarios(id,nombre,correo,password,nombre_rol) VALUES (
-1,'mario',"mario@test.com",'$2a$10$kmXtDszCsmFGKqyg.iNDWuOL5L46mwJRPX69Wx4iErl2gJwPnQx4K',"USER") ON CONFLICT (id) DO NOTHING;
-INSERT INTO usuarios(id,nombre,correo,password,nombre_rol) VALUES (
-2,'gabyu',"gaby@test.com",'$2a$10$kmXtDszCsmFGKqyg.iNDWuOL5L46mwJRPX69Wx4iErl2gJwPnQx4K',"TECH") ON CONFLICT (id) DO NOTHING;
+INSERT INTO usuarios (id, nombre, correo, password, nombre_rol) VALUES
+  (1, 'mario', 'mario@test.com',
+   '$2a$10$kmXtDszCsmFGKqyg.iNDWuOL5L46mwJRPX69Wx4iErl2gJwPnQx4K',
+   'USER')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO usuarios (id, nombre, correo, password, nombre_rol) VALUES
+  (2, 'gabyu', 'gaby@test.com',
+   '$2a$10$kmXtDszCsmFGKqyg.iNDWuOL5L46mwJRPX69Wx4iErl2gJwPnQx4K',
+   'TECH')
+ON CONFLICT (id) DO NOTHING;
